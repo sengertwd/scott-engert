bc13cad9deae2f0723abc67d3f300187464bfbc2
Scott Engert <sengert15@gmail.com>
2014-08-15 17:14:50 -0700

    Started setting up jekyll
    left out the _site directory
    Added layouts:
    * Default
    * Post
    Added Includes:
    * head
    * header
    Added _config.yml
    Added single post

--- gruntfile.js
+++ gruntfile.js
@@ -3,15 +3,15 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'src/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // },
     sass: {
       dist: {
         options: {
@@ -56,6 +56,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-autoprefixer');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['watch']);
 
 };
